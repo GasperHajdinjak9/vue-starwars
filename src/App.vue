590861dfd0e63498ecf59be6535f242b203ccbd6
GasperHajdinjak9 <gasper.hajdinjak@creatim.com>
2023-09-27 15:01:42 +0200

    inital commit

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<script lang="ts">
+    import Card from './components/Card.vue'
+
+    export default {
+        name: 'App',
+        components: {
+          Card
+        }
+    }
+    
+</script>
+
+<template>
+  <header>
+    <h1>Hello</h1>
+    <Card/>
+  </header>
+
+</template>
+
+<style scoped>
+
+</style>
---
+++ src/components/Card.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+        <h1>Card</h1>
+    </div>
+</template>
+
+<script lang="ts">
+
+    export default {
+        name: 'Card',
+    }
+    
+</script>
+
+<style scoped>
+
+</style>
