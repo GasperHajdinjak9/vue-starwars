0529139ba9da0d7293bf6d4f9190a72ffff62a85
GasperHajdinjak9 <gasper.hajdinjak@creatim.com>
2023-10-11 09:19:37 +0200

    local + indexdb storage

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -1,11 +1,11 @@
 <template>
     <Loading v-if="isLoading" />
-    <div v-else class="card-container flex flex-wrap justify-center">
-        <div v-for="character in characters" :key="character.name"
+    <div v-else class="card-container flex flex-wrap justify-center pt-[80px]">
+        <div v-for="character in characters" :key="character.id"
             class="card-container__card m-4 w-full md:w-1/2 lg:w-1/3 xl:w-1/4">
             <div class="c-card__image-container">
-                <img :src="images[character.originalName as keyof typeof images]" class="c-card__image w-full h-auto"
-                    :alt="character.name + ' Image'" />
+                <img :src="images[character.originalName as keyof typeof images]"
+                    class="c-card__image w-full h-[420px] sm:h-auto" :alt="character.name + ' Image'" />
                 <div class="c-card__text absolute bottom-0 left-0 w-full text-white p-2">
                     <!-- ime -->
                     <div v-if="!character.isEditing">
@@ -55,9 +55,17 @@
                         Gender:<input v-model="character.gender"
                             v-bind:class="{ 'bg-transparent border-b-2 border-white text-white placeholder-white::opacity-50 p-1 w-full': character.isEditing }" />
                     </div>
-                    <button @click="character.isEditing ? saveEdits(character) : startEditing(character)">
-                        {{ character.isEditing ? 'Save' : 'Edit' }}
-                    </button>
+
+                    <div class="flex justify-center mt-4">
+                        <button @click="character.isEditing ? saveEdits(character) : startEditing(character)"
+                            class="bg-yellow-400 hover:bg-yellow-500 rounded-md p-2 px-4 text-black font-extrabold">
+                            {{ character.isEditing ? 'Save' : 'Edit' }}
+                        </button>
+                        <button v-if="character.isEditing" @click="cancelEditing(character)"
+                            class="bg-red-400 hover:bg-red-500 rounded-md p-2 px-4 ml-1 text-white font-extrabold">
+                            Cancel
+                        </button>
+                    </div>
                 </div>
             </div>
         </div>
@@ -66,9 +74,8 @@
 
 
 <script lang="ts">
-import * as Vue from 'vue'
+///////* IMPORTS */////////
 import axios from 'axios'
-import VueAxios from 'vue-axios'
 
 import { defineComponent, ref, onMounted } from 'vue';
 import Loading from '../components/Loading.vue'
@@ -76,9 +83,11 @@ import Loading from '../components/Loading.vue'
 import obiWanKenobiImg from '../assets/obi-wan.png';
 import darthVaderImg from '../assets/darth-vader.png';
 import R2D2Img from '../assets/r2-d2.png';
+////////* /IMPORTS */////////
 
-
+///////* DEFINED OBJECTS *///////
 interface Character {
+    id?: number;
     name: string,
     height: string,
     mass: string,
@@ -97,6 +106,14 @@ interface Images {
     [key: string]: string;
 }
 
+const images = ref<Images>({
+    'Obi-Wan Kenobi': obiWanKenobiImg,
+    'Darth Vader': darthVaderImg,
+    'R2-D2': R2D2Img,
+});
+
+///////* DEFINED OBJECTS *///////
+
 export default defineComponent({
     name: 'Card',
     components: {
@@ -107,33 +124,83 @@ export default defineComponent({
         const characters = ref<EditableCharacter[]>([]);
         const isLoading = ref(true);
 
+        ///////////* INDEX DB *//////////////
+        let db: IDBDatabase | null = null;
+        let objectStore = null;
+        let dbOpenReq = indexedDB.open('characters', 8); //ustvari nov databse z imenom 'characters' + verzija 
 
+        dbOpenReq.addEventListener('error', (err) => {
+            console.warn(err);
+        })
+
+        dbOpenReq.addEventListener('success', (ev: Event) => {
+            db = (ev.target as IDBOpenDBRequest).result;
+        })
+
+        dbOpenReq.addEventListener('upgradeneeded', (ev: IDBVersionChangeEvent) => {
+            db = (ev.target as IDBOpenDBRequest).result;
+            let oldVersion = ev.oldVersion;
+            let newVersion = ev.newVersion || db.version;
+            console.log('upgraded from', oldVersion, 'to', newVersion)
+
+            //preverimo, če object že obstaja, če ne, ustvarimo db object in določimo/dodamo key
+
+            if (!db.objectStoreNames.contains('characters')) {
+
+                objectStore = db.createObjectStore('characters', {
+                    keyPath: 'id',
+                    autoIncrement: true
+                })
+            }
+        })
+
+        const addToIndexDB = (character: EditableCharacter) => {
+            // zbrišemo "isEditing" zaradi težave pri kloniranju objecta
+            const clonedCharacter = { ...character };
+            delete clonedCharacter.isEditing;
+
+            // določimo specifičen store v bazi
+            let tx = db!.transaction('characters', 'readwrite');
+            let store = tx.objectStore('characters');
+
+            //dodamo character v object store
+            store.put(clonedCharacter);
+
+            tx.oncomplete = () => {
+                console.log('Stored character:', clonedCharacter);
+            };
+
+            tx.onerror = (event: Event) => {
+                console.error('Error storing character:', event);
+            };
+        }
+
+        ///////////* /INDEX DB *///////////
+
+
+        //////////* EDITING FUNCTIONALITIES *//////////
+
+        const originalCharacters = ref<EditableCharacter[]>([]);
 
         const startEditing = (character: EditableCharacter) => {
             character.isEditing = true;
         };
 
-        const saveEdits = (character: EditableCharacter) => {
+        const cancelEditing = (character: EditableCharacter) => {
             character.isEditing = false;
         };
 
+        const saveEdits = (character: EditableCharacter) => {
+            character.isEditing = false;
 
-
-
-
-        const images = ref<Images>({
-            'Obi-Wan Kenobi': obiWanKenobiImg,
-            'Darth Vader': darthVaderImg,
-            'R2-D2': R2D2Img,
-        });
-
-
-        const toggleEdit = (character: EditableCharacter) => {
-            character.isEditing = !character.isEditing;
+            // odkomentiraj glede na kater browser storage želiš uporabit
+            // localStorage.setItem("data", JSON.stringify(characters.value)) // store to local storage
+            // addToIndexDB(character) // store to indexDB
         };
 
+         //////////* /EDITING FUNCTIONALITIES *//////////
 
-
+        let uniqueId = 1;
         const fetchCharacters = async () => {
             try {
                 isLoading.value = true;
@@ -141,7 +208,7 @@ export default defineComponent({
                 const selectedCharacters = ['R2-D2', 'Darth Vader', 'Obi-Wan Kenobi',];
                 characters.value = response.data.results
                     .filter((char: EditableCharacter) => selectedCharacters.includes(char.name))
-                    .map((char: EditableCharacter) => ({ ...char, isEditing: false, originalName: char.name }));
+                    .map((char: EditableCharacter) => ({ ...char, isEditing: false, originalName: char.name, id: uniqueId++, }));
                 isLoading.value = false;
 
             } catch (err) {
@@ -150,27 +217,34 @@ export default defineComponent({
             }
         }
 
-        onMounted(fetchCharacters);
+        //////////* LOCAL STORAGE *//////////
+        const storedData = JSON.parse(localStorage.getItem("data") || '[]');
+
+        if (storedData.length > 0) {
+            characters.value = storedData;
+            isLoading.value = false;
+        } else {
+            onMounted(fetchCharacters);
+        }
+        //////////* /LOCAL STORAGE *//////////
 
         return {
             characters,
             images,
-            toggleEdit,
             startEditing,
+            cancelEditing,
             saveEdits,
             isLoading
         }
     },
 });
-
-
-
 </script>
 
 <style scoped lang="scss">
 .c-card {
     &__image-container {
         position: relative;
+        z-index: 10;
     }
 
     &__image {
@@ -193,6 +267,5 @@ export default defineComponent({
     display: flex;
     justify-content: center;
     align-items: center;
-
 }
 </style>
--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,23 +1,29 @@
 <template>
     <nav class="navbar">
-        <a href="" class="navbar__link">Upcoming</a>
-        <a href="" class="navbar__link">Legacy</a>
-        <img src="../assets/star-wars-logo.png" alt="" class="navbar__logo w-40 h-15">
-        <a href="" class="navbar__link">Merch</a>
-        <a href="" class="navbar__link">About</a>
-
+        <button class="navbar__hamburger" @click="toggleMenu">&#9776;</button> 
+        <div class="navbar__links-container" :class="{ 'active': menuOpen }">
+            <a href="" class="navbar__link">Upcoming</a>
+            <a href="" class="navbar__link">Legacy</a>
+            <img src="../assets/star-wars-logo.png" alt="" class="navbar__logo w-40 h-15">
+            <a href="" class="navbar__link">Merch</a>
+            <a href="" class="navbar__link">About</a>
+        </div>
     </nav>
 </template>
 
-<script lang="ts">
-import { defineComponent } from 'vue'
 
+<script lang="ts">
+import { defineComponent, ref } from 'vue'
 
 export default defineComponent({
     setup() {
+        const menuOpen = ref(false);
 
+        const toggleMenu = () => {
+            menuOpen.value = !menuOpen.value;
+        };
 
-        return {}
+        return { menuOpen, toggleMenu }
     }
 })
 </script>
@@ -25,11 +31,36 @@ export default defineComponent({
 <style scoped lang="scss">
 .navbar {
     display: flex;
-    justify-content: space-between;
+    justify-content: center;
     align-items: center;
     padding: 0 20px;
     max-height: 80px;
     background-color: transparent;
+    position: fixed;
+    top: 0;
+    left: 0;
+    width: 100%;
+
+    &__hamburger {
+        display: none;
+        font-size: 30px;
+        background-color: transparent;
+        border: none;
+        color: white;
+    }
+
+    &__links-container {
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+        padding: 50px 0;
+        width: 100%;
+        max-width: 800px;
+
+        &.active {
+            display: flex;
+        }
+    }
 }
 
 .navbar__link {
@@ -46,6 +77,40 @@ export default defineComponent({
     font-size: 24px;
     font-weight: bold;
     color: gold;
+}
+
+@media (max-width: 768px) {
+    .navbar {
+        justify-content: right;
+        z-index: 9999999;
+        background-color: #000;
+        &__logo {
+            display: none;
+        } 
+    }
+    .navbar__links-container {
+        display: none;
+        flex-direction: column;
+        position: absolute;
+        top: 100%;
+        left: 0;
+        background-color: #000;
+        width: 100%;
+        line-height: 5rem;
+        a {
+            &:hover {
+                background-color: rgb(0, 0, 0, 0.7);
+            }
+        }
+    }
+
+    .navbar__links-container.active {
+        display: flex;
+    }
+
+    .navbar__hamburger {
+        display: block;
+    }
 }
 
 </style>
